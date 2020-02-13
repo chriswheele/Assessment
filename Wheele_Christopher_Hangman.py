32b76608d91c7518619679ed42d59d26ff6b48f6
chriswheele <61020406+chriswheele@users.noreply.github.com>
2020-02-13 20:59:26 +0000

    Create Wheele_Christopher_Hangman.py

---
+++ Wheele_Christopher_Hangman.py
@@ -0,0 +1,76 @@
+
+
+
+
+word = random.choice(["rarely", "universe", "notice", "sugar", "interference", "constitution", "we", "minus"])
+
+
+
+#display code section 
+
+def main_frame(attempt, wd):
+ if (attempt == 0):
+  print("    _____     ")
+  print("   |     |    ")
+  print("   |          ")
+  print("   |          ")
+  print("   |          ")
+  print("   |          ")
+  print("   |          ")
+  print("--------------")
+ elif (attempt == 1):
+  print("    _____     ")
+  print("   |     |    ")
+  print("   |     O    ")
+  print("   |          ")
+  print("   |          ")
+  print("   |          ")
+  print("   |          ")
+  print("--------------")
+ elif (attempt == 2):
+  print("    _____     ")
+  print("   |     |    ")
+  print("   |     O    ")
+  print("   |     |    ")
+  print("   |     |    ")
+  print("   |          ")
+  print("   |          ")
+  print("--------------")
+ elif (attempt == 3):
+  print("    _____     ")
+  print("   |     |    ")
+  print("   |     O    ")
+  print("   |     |    ")
+  print("   |     |    ")
+  print("   |    /     ")
+  print("   |          ")
+  print("--------------")
+ elif (attempt == 4):
+  print("    _____     ")
+  print("   |     |    ")
+  print("   |     O    ")
+  print("   |     |    ")
+  print("   |     |    ")
+  print("   |    / \   ")
+  print("   |          ")
+  print("--------------")
+elif (attempt == 5):
+  print("    _____     ")
+  print("   |     |    ")
+  print("   |     O    ")
+  print("   |   _ |    ")
+  print("   |     |    ")
+  print("   |    / \   ")
+  print("   |          ")
+  print("--------------")
+ elif (attempt == 6):
+  print("    _____     ")
+  print("   |     |    ")
+  print("   |     O    ")
+  print("   |   _ | _  ")
+  print("   |     |    ")
+  print("   |    / \   ")
+  print("   |          ")
+  print("--------------")
+  n/
+  
