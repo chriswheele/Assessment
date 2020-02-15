013f4264f034ed81df673249dbd214ebf797458c
chriswheele <61020406+chriswheele@users.noreply.github.com>
2020-02-15 17:59:21 +0000

    Update Wheele_Christopher_Hangman.py

--- Wheele_Christopher_Hangman.py
+++ Wheele_Christopher_Hangman.py
@@ -1,74 +1,115 @@
-def chooseword(): 
-list = open('words')
-word = words.readlines()
-print(word)
 
+def displayed_game(attempt, wd):
+    if (attempt == 0):
+      print("    _____     ")
+      print("   |     |    ")
+      print("   |          ")
+      print("   |          ")
+      print("   |          ")
+      print("   |          ")
+      print("   |          ")
+      print("--------------")
+    elif (attempt == 1):
+      print("    _____     ")
+      print("   |     |    ")
+      print("   |     O    ")
+      print("   |          ")
+      print("   |          ")
+      print("   |          ")
+      print("   |          ")
+       print("--------------")
+    elif (attempt == 2):
+      print("    _____     ")
+      print("   |     |    ")
+      print("   |     O    ")
+      print("   |     |    ")
+      print("   |     |    ")
+      print("   |          ")
+      print("   |          ")
+      print("--------------")
+      elif (attempt == 3):
+      print("    _____     ")
+      print("   |     |    ")
+      print("   |     O    ")
+      print("   |     |    ")
+      print("   |     |    ")
+      print("   |    /     ")
+      print("   |          ")
+      print("--------------")
+    elif (attempt == 4):
+      print("    _____     ")
+      print("   |     |    ")
+      print("   |     O    ")
+      print("   |     |    ")
+      print("   |     |    ")
+      print("   |    / \   ")
+      print("   |          ")
+      print("--------------")
+    elif (attempt == 5):
+      print("    _____     ")
+      print("   |     |    ")
+      print("   |     O    ")
+      print("   |   _ |    ")
+      print("   |     |    ")
+      print("   |    / \   ")
+      print("   |          ")
+      print("--------------")
+    elif (attempt == 6):
+      print("    _____     ")
+      print("   |     |    ")
+      print("   |     O    ")
+      print("   |   _ | _  ")
+      print("   |     |    ")
+      print("   |    / \   ")
+      print("   |          ")
+      print("--------------")
+return True
 
-#display code section 
+#choose a word to play
 
-def main_frame(attempt, wd):
- if (attempt == 0):
-  print("    _____     ")
-  print("   |     |    ")
-  print("   |          ")
-  print("   |          ")
-  print("   |          ")
-  print("   |          ")
-  print("   |          ")
-  print("--------------")
- elif (attempt == 1):
-  print("    _____     ")
-  print("   |     |    ")
-  print("   |     O    ")
-  print("   |          ")
-  print("   |          ")
-  print("   |          ")
-  print("   |          ")
-  print("--------------")
- elif (attempt == 2):
-  print("    _____     ")
-  print("   |     |    ")
-  print("   |     O    ")
-  print("   |     |    ")
-  print("   |     |    ")
-  print("   |          ")
-  print("   |          ")
-  print("--------------")
- elif (attempt == 3):
-  print("    _____     ")
-  print("   |     |    ")
-  print("   |     O    ")
-  print("   |     |    ")
-  print("   |     |    ")
-  print("   |    /     ")
-  print("   |          ")
-  print("--------------")
- elif (attempt == 4):
-  print("    _____     ")
-  print("   |     |    ")
-  print("   |     O    ")
-  print("   |     |    ")
-  print("   |     |    ")
-  print("   |    / \   ")
-  print("   |          ")
-  print("--------------")
-elif (attempt == 5):
-  print("    _____     ")
-  print("   |     |    ")
-  print("   |     O    ")
-  print("   |   _ |    ")
-  print("   |     |    ")
-  print("   |    / \   ")
-  print("   |          ")
-  print("--------------")
- elif (attempt == 6):
-  print("    _____     ")
-  print("   |     |    ")
-  print("   |     O    ")
-  print("   |   _ | _  ")
-  print("   |     |    ")
-  print("   |    / \   ")
-  print("   |          ")
-  print("--------------")
-  n/
+def choose_word():
+ my_word = "hangman"
+ hidden_word = my_word
+return hidden_word
+
+#Hangman game code
+
+def run_hangman():
+  print("Hangman Game")
+  hidden_word = choose_word()
+  word_template = list("*"*len(hidden_word))
+  word_characters = list(solution_word)
+  attempt = 7
+  number_guesses = []
   
+  while lives >= 0:
+   if word_template == word_characters
+    print("".join(word_template))
+    break
+   hung = hangman_frames(attempt)
+   if hung == True
+    break
+   valid_guess = False
+   while valid_guess == False
+    print("Lives remaining = " + str(lives))
+    print("".join(word_template))
+    guess = input("please enter your next guess: ")
+    allowed_guesses = "abcdefghijklmnopqrstuvwxyz"
+    if guess.lower() not in allowed allowed_guesses or len(guess) > 1:
+      print("Invalid guess - must be single letter")
+      valid_guess = False
+    elif guess.lower() in guessed_letters:
+      print("Invalid guess - letter has already been used")
+      valid_guess = False
+    else:
+      valid_guess = True
+   guess = guess.lower()
+   guessed_letters.oppened(guess))
+   correct = False
+   for i in range(len(solution_word)):
+     if guess == word_characters[i]:
+       word_template[i] = word_characters[i]
+       correct = True
+   if correct == True
+    print("Correct Guess")
+      
